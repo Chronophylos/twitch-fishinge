c7d9ddec36d642004e1b6f6286e15c463354e8b0
Chronophylos <nikolai@chronophylos.com>
2022-08-26 13:48:22 +0200

    change template engine to tera

--- src/bin/webserver/main.rs
+++ src/bin/webserver/main.rs
@@ -1,19 +1,16 @@
-use std::fmt::Write;
+use std::collections::HashMap;
 
 use futures_lite::future::block_on;
 use log::error;
+use once_cell::sync::Lazy;
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use tinytemplate::{format, TinyTemplate};
+use tera::{Context, Tera, Value};
 use twitch_fishinge::{
     db_conn,
     models::{Fish, User},
 };
 use warp::{reply::Html, Filter, Reply};
-
-const LEADERBOARD: &str = include_str!("html/leaderboard.html");
-const FISHES: &str = include_str!("html/fishes.html");
 
 #[derive(Debug, thiserror::Error)]
 enum Error {
@@ -26,26 +23,31 @@ enum Error {
     #[error("Could not query fishes")]
     QueryFishes(#[source] sqlx::Error),
 
-    #[error("Could not compile template")]
-    CompileTemplate(#[source] tinytemplate::error::Error),
-
     #[error("Could not render template")]
-    RenderTemplate(#[source] tinytemplate::error::Error),
+    RenderTemplate(#[source] tera::Error),
 }
 
-fn score_formatter(value: &Value, output: &mut String) -> Result<(), tinytemplate::error::Error> {
+fn score_formatter(value: &Value, _args: &HashMap<String, Value>) -> tera::Result<Value> {
     match value {
-        Value::Number(n) if n.is_f64() => {
-            write!(output, "${:.2}", n.as_f64().unwrap())?;
-            Ok(())
-        }
-        Value::Number(n) if n.is_i64() => {
-            write!(output, "${}.00", n.as_i64().unwrap())?;
-            Ok(())
-        }
-        _ => format(value, output),
+        Value::Number(n) if n.is_f64() => Ok(Value::String(format!("${:.2}", n.as_f64().unwrap()))),
+        Value::Number(n) if n.is_i64() => Ok(Value::String(format!("${}.00", n.as_i64().unwrap()))),
+        _ => Err(tera::Error::msg("Could not format score")),
     }
 }
+
+fn percentage_formatter(value: &Value, _args: &HashMap<String, Value>) -> tera::Result<Value> {
+    match value {
+        Value::Number(n) if n.is_f64() => Ok(Value::String(format!("{:.2}%", n.as_f64().unwrap()))),
+        _ => Err(tera::Error::msg("Could not format percentage")),
+    }
+}
+
+static TEMPLATES: Lazy<Tera> = Lazy::new(|| {
+    let mut tera = Tera::new("templates/**/*.html").unwrap();
+    tera.register_filter("score", score_formatter);
+    tera.register_filter("percentage", percentage_formatter);
+    tera
+});
 
 #[derive(Serialize, Deserialize, Default)]
 #[serde(default)]
@@ -54,17 +56,7 @@ struct LeaderboardQuery {
 }
 
 async fn leaderboard(query: LeaderboardQuery) -> Result<Html<String>, Error> {
-    #[derive(Serialize)]
-    struct Context {
-        users: Vec<(usize, User)>,
-    }
-
     let mut conn = db_conn().await?;
-
-    let mut tt = TinyTemplate::new();
-    tt.add_template("leaderboard", LEADERBOARD)
-        .map_err(Error::CompileTemplate)?;
-    tt.add_formatter("score_formatter", score_formatter);
 
     let users: Vec<_> = sqlx::query_as!(User, "SELECT * FROM users ORDER BY score DESC")
         .fetch_all(&mut conn)
@@ -73,24 +65,19 @@ async fn leaderboard(query: LeaderboardQuery) -> Result<Html<String>, Error> {
         .into_iter()
         .filter(|user| user.score > 0.0)
         .filter(|user| !user.is_bot || query.include_bots)
-        .enumerate()
-        .map(|(i, user)| (i + 1, user))
         .collect();
 
-    let context = Context { users };
+    let mut context = Context::new();
+    context.insert("users", &users);
 
     Ok(warp::reply::html(
-        tt.render("leaderboard", &context)
+        TEMPLATES
+            .render("leaderboard.html", &context)
             .map_err(Error::RenderTemplate)?,
     ))
 }
 
 async fn fishes() -> Result<Html<String>, Error> {
-    #[derive(Serialize)]
-    struct Context {
-        fishes: Vec<Row>,
-    }
-
     #[derive(Serialize)]
     struct Row {
         name: String,
@@ -103,18 +90,6 @@ async fn fishes() -> Result<Html<String>, Error> {
     }
 
     let mut conn = db_conn().await?;
-
-    let mut tt = TinyTemplate::new();
-    tt.add_template("fishes", FISHES)
-        .map_err(Error::CompileTemplate)?;
-    tt.add_formatter("score_formatter", score_formatter);
-    tt.add_formatter("percentage_formatter", |value, output| match value {
-        Value::Number(n) if n.is_f64() => {
-            write!(output, "{:.2}%", n.as_f64().unwrap() * 100.0)?;
-            Ok(())
-        }
-        _ => format(value, output),
-    });
 
     let fishes = sqlx::query_as!(Fish, "SELECT * FROM fishes")
         .fetch_all(&mut conn)
@@ -139,10 +114,20 @@ async fn fishes() -> Result<Html<String>, Error> {
     rows.sort_by_key(|row| (row.chance * 10000.0) as u64);
     rows.reverse();
 
-    let context = Context { fishes: rows };
+    let mut context = Context::new();
+    context.insert("fishes", &rows);
 
     Ok(warp::reply::html(
-        tt.render("fishes", &context)
+        TEMPLATES
+            .render("fishes.html", &context)
+            .map_err(Error::RenderTemplate)?,
+    ))
+}
+
+fn index() -> Result<Html<String>, Error> {
+    Ok(warp::reply::html(
+        TEMPLATES
+            .render("index.html", &Context::new())
             .map_err(Error::RenderTemplate)?,
     ))
 }
@@ -174,7 +159,16 @@ async fn main() -> Result<(), Error> {
     pretty_env_logger::init();
 
     // GET /
-    let root = warp::path::end().map(|| warp::reply::html(include_str!("html/index.html")));
+    let root = warp::path::end().map(|| match index() {
+        Ok(html) => Box::new(html) as Box<dyn Reply>,
+        Err(err) => {
+            error!("Could not render index: {:?}", err);
+            Box::new(warp::reply::with_status(
+                warp::reply(),
+                StatusCode::INTERNAL_SERVER_ERROR,
+            )) as Box<dyn Reply>
+        }
+    });
 
     // GET /leaderboard
     let leaderboard_route = warp::path("leaderboard")
@@ -217,7 +211,7 @@ async fn main() -> Result<(), Error> {
         "site.webmanifest" => "application/manifest+json",
         "Neonderthaw-Regular.ttf" => "application/x-font-ttf",
         "Righteous-Regular.ttf" => "application/x-font-ttf",
-        "styles.css" => "text/css"
+        "style.css" => "text/css"
     };
 
     let routes = warp::get()
