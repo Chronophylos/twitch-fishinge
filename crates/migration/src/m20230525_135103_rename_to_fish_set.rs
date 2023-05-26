7c6fb23ec23fb4c85bfdb10f4f607e862f74c3b8
Chronophylos <nikolai@chronophylos.com>
2023-05-26 16:33:06 +0200

    fix migrations

--- crates/migration/src/m20230525_135103_rename_to_fish_set.rs
+++ crates/migration/src/m20230525_135103_rename_to_fish_set.rs
@@ -1,78 +1,135 @@
 use sea_orm_migration::prelude::*;
 
-const QUERY_BUILDER: PostgresQueryBuilder = PostgresQueryBuilder;
-
 #[derive(DeriveMigrationName)]
 pub struct Migration;
-
-macro_rules! debug_query {
-    ($query:expr) => {
-        match $query {
-            val => {
-                eprintln!(
-                    "[{}:{}]: {}",
-                    file!(),
-                    line!(),
-                    val.to_string(QUERY_BUILDER)
-                );
-                val
-            }
-        }
-    };
-}
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // create set table
         manager
-            .create_table(debug_query!(Table::create()
-                .table(Bundle::Table)
-                .col(
-                    ColumnDef::new(Bundle::Id)
-                        .primary_key()
-                        .auto_increment()
-                        .not_null()
-                        .integer(),
-                )
-                .take()))
+            .create_table(
+                Table::create()
+                    .table(Bundle::Table)
+                    .col(
+                        ColumnDef::new(Bundle::Id)
+                            .primary_key()
+                            .auto_increment()
+                            .not_null()
+                            .integer(),
+                    )
+                    .take(),
+            )
             .await?;
 
         // rename fishesseason to fishset
         manager
-            .rename_table(debug_query!(Table::rename()
-                .table(FishesSeasons::Table, FishBundle::Table)
-                .take()))
+            .rename_table(
+                Table::rename()
+                    .table(FishesSeasons::Table, FishBundle::Table)
+                    .take(),
+            )
             .await?;
 
-        // rename fishset.season_id to set_id and make it a foreign key
+        // rename fishset.season_id to set_id
         manager
-            .alter_table(debug_query!(Table::alter()
-                .table(FishBundle::Table)
-                .rename_column(FishBundle::SeasonId, FishBundle::BundleId)
-                .drop_foreign_key(Alias::new("FK_fishes_seasons_season_id"))
-                .add_foreign_key(
-                    TableForeignKey::new()
-                        .name("FK_fishset_set_id")
-                        .from_tbl(FishBundle::Table)
-                        .from_col(FishBundle::BundleId)
-                        .to_tbl(Bundle::Table)
-                        .to_col(Bundle::Id),
-                )
-                .take()))
+            .alter_table(
+                Table::alter()
+                    .table(FishBundle::Table)
+                    .rename_column(FishBundle::SeasonId, FishBundle::BundleId)
+                    .take(),
+            )
+            .await?;
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(FishBundle::Table)
+                    .drop_foreign_key(Alias::new("FK_fishes_seasons_season_id"))
+                    .add_foreign_key(
+                        TableForeignKey::new()
+                            .name("FK_fishbundle_bundle_id")
+                            .from_tbl(FishBundle::Table)
+                            .from_col(FishBundle::BundleId)
+                            .to_tbl(Bundle::Table)
+                            .to_col(Bundle::Id),
+                    )
+                    .take(),
+            )
+            .await?;
+
+        // create empty bundle
+        manager
+            .exec_stmt(
+                Query::insert()
+                    .into_table(Bundle::Table)
+                    .columns([Bundle::Id])
+                    .values_panic(vec![0.into()])
+                    .to_owned(),
+            )
+            .await?;
+
+        // add bundle_id column to season
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Seasons::Table)
+                    .add_column(
+                        ColumnDef::new(Seasons::BundleId)
+                            .integer()
+                            .not_null()
+                            .default(0),
+                    )
+                    .add_foreign_key(
+                        TableForeignKey::new()
+                            .name("FK_seasons_bundle_id")
+                            .from_tbl(Seasons::Table)
+                            .from_col(Seasons::Id)
+                            .to_tbl(Bundle::Table)
+                            .to_col(Bundle::Id),
+                    )
+                    .take(),
+            )
             .await?;
 
         Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // revert add bundle_id column to season
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Seasons::Table)
+                    .drop_foreign_key(Alias::new("FK_seasons_bundle_id"))
+                    .drop_column(Seasons::BundleId)
+                    .take(),
+            )
+            .await?;
+
+        // revert create empty bundle
+        manager
+            .exec_stmt(
+                Query::delete()
+                    .from_table(Bundle::Table)
+                    .and_where(Expr::col(Bundle::Id).eq(0))
+                    .to_owned(),
+            )
+            .await?;
+
         // revert rename fishset.season_id to set_id and make it a foreign key
         manager
             .alter_table(
                 Table::alter()
                     .table(FishBundle::Table)
                     .rename_column(FishBundle::BundleId, FishBundle::SeasonId)
-                    .drop_foreign_key(Alias::new("FK_fishset_set_id"))
+                    .take(),
+            )
+            .await?;
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(FishBundle::Table)
+                    .drop_foreign_key(Alias::new("FK_fishbundle_bundle_id"))
                     .add_foreign_key(
                         TableForeignKey::new()
                             .name("FK_fishes_seasons_season_id")
@@ -85,7 +142,7 @@ impl MigrationTrait for Migration {
             )
             .await?;
 
-        // revert rename fishesseason to fishset
+        // revert rename fishesseason to fishbundle
         manager
             .rename_table(
                 Table::rename()
@@ -94,7 +151,7 @@ impl MigrationTrait for Migration {
             )
             .await?;
 
-        // revert create set table
+        // revert create bundle table
         manager
             .drop_table(Table::drop().table(Bundle::Table).take())
             .await?;
@@ -126,4 +183,5 @@ enum Bundle {
 enum Seasons {
     Table,
     Id,
+    BundleId,
 }
