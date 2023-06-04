ede7545f2dc505ee488239551cc1d1b03a1ae822
Chronophylos <nikolai@chronophylos.com>
2023-06-04 19:50:51 +0200

    finally

--- crates/supinic-fish-bot/src/bot_runner.rs
+++ crates/supinic-fish-bot/src/bot_runner.rs
@@ -1,13 +1,13 @@
-use std::{collections::HashSet, sync::Arc};
+use std::{collections::HashSet, future::Future, pin::Pin, sync::Arc};
 
 use database::connection;
-use dotenvy::dotenv;
 use log::{debug, error, info};
-use miette::{Diagnostic, Result, WrapErr};
+use miette::{Diagnostic, Result};
 use sea_orm::DatabaseConnection;
 use signal_hook::consts::signal::{SIGINT, SIGQUIT, SIGTERM};
 use signal_hook_tokio::Signals;
-use tokio::{select, sync::Notify};
+use tokio::{select, sync::Notify, task::JoinHandle};
+use tokio_stream::StreamExt;
 use twitch_irc::{
     login::RefreshingLoginCredentials, message::ServerMessage, ClientConfig, SecureTCPTransport,
     TwitchIRCClient,
@@ -37,120 +37,167 @@ pub enum Error {
     #[error("could not get account")]
     #[diagnostic(code(bot_runner::get_account))]
     GetAccount(#[source] account::Error),
+
+    #[error("could not set wanted channels")]
+    #[diagnostic(code(bot_runner::set_wanted_channels))]
+    SetWantedChannels(#[source] twitch_irc::validate::Error),
+
+    #[error("failed to run twitch task")]
+    #[diagnostic(code(bot_runner::twitch_task))]
+    TwitchTask(#[source] tokio::task::JoinError),
+
+    #[error("failed to run signals task")]
+    #[diagnostic(code(bot_runner::signals_task))]
+    SignalsTask(#[source] tokio::task::JoinError),
 }
 
-#[async_trait::async_trait]
-pub trait Bot {
-    fn run(&self, db: &DatabaseConnection, quit_signal: Arc<Notify>) -> Result<()>;
-
-    async fn handle_server_message(
-        &self,
-        db: &DatabaseConnection,
-        client: &Client,
-        message: ServerMessage,
-    ) -> Result<()>;
-}
-
-pub struct BotRunner<B>
+pub async fn start_bot<F>(handle_server_message: F) -> Result<()>
 where
-    B: Bot,
+    F: Fn(
+            DatabaseConnection,
+            Client,
+            ServerMessage,
+        ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>>
+        + Send
+        + Sync
+        + 'static,
 {
-    signals: Signals,
-    quit_signal: Arc<Notify>,
-    database_connection: DatabaseConnection,
-    signals_task: tokio::task::JoinHandle<()>,
-    bot_task: tokio::task::JoinHandle<()>,
-    bot: B,
+    let (quit, quit_handle, quit_task) = register_signals()?;
+
+    info!("Connecting to database");
+    let conn = connection().await.map_err(Error::ConnectDatabase)?;
+
+    let wanted_channels = env_var("CHANNELS")?
+        .split(',')
+        .map(|channel| channel.trim().to_string())
+        .collect::<HashSet<_>>();
+
+    let twitch_task =
+        start_twitch_bot(conn.clone(), wanted_channels, quit, handle_server_message).await?;
+
+    // keep the tokio executor alive.
+    // If you return instead of waiting the background task will exit.
+    twitch_task.await.map_err(Error::TwitchTask)?;
+
+    // Terminate the signal stream.
+    quit_handle.close();
+    quit_task.await.map_err(Error::SignalsTask)?;
+
+    Ok(())
 }
 
-impl<B> BotRunner<B>
+fn register_signals() -> Result<(Arc<Notify>, signal_hook_tokio::Handle, JoinHandle<()>), Error> {
+    info!("Registering signals");
+
+    let signals = Signals::new(&[SIGINT, SIGTERM, SIGQUIT]).map_err(Error::RegisterSignals)?;
+    let notify = Arc::new(Notify::new());
+
+    let handle = signals.handle();
+    let task = tokio::spawn(handle_signals(signals, notify.clone()));
+
+    Ok((notify, handle, task))
+}
+
+async fn start_twitch_bot<F>(
+    conn: DatabaseConnection,
+    wanted_channels: HashSet<String>,
+    quit: Arc<Notify>,
+    handle_server_message: F,
+) -> Result<JoinHandle<()>, Error>
 where
-    B: Bot + Send + Sync + Clone + 'static,
+    F: Fn(
+            DatabaseConnection,
+            Client,
+            ServerMessage,
+        ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>>
+        + Send
+        + Sync
+        + 'static,
 {
-    pub async fn new(bot: B) -> Result<Self, Error>
-    where
-        B: Bot,
-    {
-        let signals = Signals::new(&[SIGINT, SIGTERM, SIGQUIT]).map_err(Error::RegisterSignals)?;
-        let quit_signal = Arc::new(Notify::new());
+    info!("Creating twitch client");
 
-        info!("connecting to database");
-        let db = connection().await.map_err(Error::ConnectDatabase)?;
-        let config = Self::create_client_config(&db).await?;
+    let config = create_client_config(&conn).await?;
+    let (mut incoming_messages, client) = Client::new(config);
 
-        info!("creating twitch client");
-        let (mut incoming_messages, client) = Client::new(config);
+    let twitch_task = tokio::spawn({
+        let client = client.clone();
 
-        let twitch_task = tokio::spawn({
-            let client = client.clone();
-            let bot = bot.clone();
-
-            async move {
-                loop {
-                    select! {
-                        maybe_message = incoming_messages.recv() => {
-                            if let Some(message) = maybe_message {
-                                if let Err(err) = bot.handle_server_message(&db, &client, message).await {
-                                    error!("Error handling message: {err}");
-                                }
-
-                            } else {
-                                break;
+        async move {
+            loop {
+                select! {
+                    maybe_message = incoming_messages.recv() => {
+                        if let Some(message) = maybe_message {
+                            if let Err(err) = handle_server_message(conn.clone(), client.clone(), message).await {
+                                error!("Error handling message: {err}");
                             }
-                        }
-                        _ = quit_signal.notified() => {
-                            debug!("Received quitting twitch task");
+
+                        } else {
                             break;
                         }
                     }
+                    _ = quit.notified() => {
+                        debug!("Received quitting twitch task");
+                        break;
+                    }
                 }
             }
-        });
+        }
+    });
 
-        let wanted_channels = env_var("CHANNELS")?
-            .split(',')
-            .map(|channel| channel.trim().to_string())
-            .collect::<HashSet<_>>();
+    debug!(
+        "Setting wanted channels: {}",
+        wanted_channels
+            .iter()
+            .map(|s| s.as_str())
+            .collect::<Vec<_>>()
+            .join(", ")
+    );
 
-        debug!(
-            "Wanting to join channels {}",
-            wanted_channels
-                .iter()
-                .map(|s| s.as_str())
-                .collect::<Vec<_>>()
-                .join(", ")
-        );
+    client
+        .set_wanted_channels(wanted_channels)
+        .map_err(Error::SetWantedChannels)?;
 
-        client.set_wanted_channels(wanted_channels)?;
+    Ok(twitch_task)
+}
 
-        todo!()
-    }
+async fn create_client_config(
+    conn: &DatabaseConnection,
+) -> Result<ClientConfig<RefreshingLoginCredentials<Account>>, Error> {
+    let username = env_var("USERNAME")?;
+    let client_id = env_var("CLIENT_ID")?;
+    let client_secret = env_var("CLIENT_SECRET")?;
 
-    async fn create_client_config(
-        db: &DatabaseConnection,
-    ) -> Result<ClientConfig<RefreshingLoginCredentials<Account>>, Error> {
-        let username = env_var("USERNAME")?;
-        let client_id = env_var("CLIENT_ID")?;
-        let client_secret = env_var("CLIENT_SECRET")?;
+    info!("creating client config for {username}");
 
-        info!("creating client config for {username}");
+    let account = Account::new(conn.clone(), &username)
+        .await
+        .map_err(Error::GetAccount)?;
+    let credentials = RefreshingLoginCredentials::init_with_username(
+        Some(username),
+        client_id,
+        client_secret,
+        account,
+    );
+    let config = ClientConfig::new_simple(credentials);
 
-        let account = Account::new(db.clone(), &username)
-            .await
-            .map_err(Error::GetAccount)?;
-        let credentials = RefreshingLoginCredentials::init_with_username(
-            Some(username),
-            client_id,
-            client_secret,
-            account,
-        );
-        let config = ClientConfig::new_simple(credentials);
-
-        Ok(config)
-    }
+    Ok(config)
 }
 
 #[inline]
 fn env_var(name: &'static str) -> Result<String, Error> {
     std::env::var(name).map_err(|source| Error::EnvVarNotSet { source, name })
+}
+
+async fn handle_signals(mut signals: Signals, quit_signal: Arc<Notify>) {
+    info!("Starting signal handler");
+    while let Some(signal) = signals.next().await {
+        match signal {
+            SIGTERM | SIGINT | SIGQUIT => {
+                // Shutdown the system
+                quit_signal.notify_waiters();
+                break;
+            }
+            _ => unreachable!(),
+        }
+    }
 }
--- crates/supinic-fish-bot/src/main.rs
+++ crates/supinic-fish-bot/src/main.rs
@@ -1,16 +1,24 @@
+use futures::future::FutureExt;
 use miette::{Result, WrapErr};
-use supinic_fish_bot::bot_runner::BotRunner;
+use sea_orm::DatabaseConnection;
+use supinic_fish_bot::bot_runner::{start_bot, Client};
+use twitch_irc::message::ServerMessage;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init_timed();
     dotenvy::dotenv().ok();
 
-    start().await.wrap_err("failed to run bot")
+    let closure = |conn: DatabaseConnection, client: Client, message: ServerMessage| {
+        handle_server_message(conn, client, message).boxed()
+    };
+    start_bot(closure).await.wrap_err("failed to run bot")
 }
 
-async fn start() -> Result<()> {
-    BotRunner::new().await?;
-
+async fn handle_server_message(
+    _conn: DatabaseConnection,
+    _client: Client,
+    _message: ServerMessage,
+) -> Result<()> {
     Ok(())
 }
